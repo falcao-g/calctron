64ead5319b5eecc3df4abf6a773730d873809cca
Falcão <gabriel.p.falcao50@gmail.com>
2022-07-30 13:56:34 -0400

    update background
    fixed period numpad key in windows, added ^ keyboard support and deleted decimal verification

--- app/js/background.js
+++ app/js/background.js
@@ -3,8 +3,6 @@ const input = document.getElementById("screen");
 const operators = ["+", "-", "*", "/", "^"];
 const math = require("mathjs");
 
-let decimalAdded = true;
-
 for (const key of keys) {
 	key.addEventListener("click", async function (e) {
 		const inputVal = input.value;
@@ -12,7 +10,6 @@ for (const key of keys) {
 
 		if (btnVal === "C") {
 			input.value = "";
-			decimalAdded = true;
 		} else if (
 			(btnVal === "√" ||
 				btnVal === "√x" ||
@@ -28,7 +25,6 @@ for (const key of keys) {
 			input.value === ""
 		) {
 			input.value = "";
-			decimalAdded = true;
 		} else if (btnVal === "=") {
 			let equation = inputVal;
 			const lastChar = equation[equation.length - 1];
@@ -43,8 +39,6 @@ for (const key of keys) {
 					.toString();
 				input.value = result;
 			}
-
-			decimalAdded = false;
 		} else if (operators.includes(btnVal)) {
 			const lastChar = inputVal[inputVal.length - 1];
 
@@ -57,45 +51,28 @@ for (const key of keys) {
 			if (operators.includes(lastChar) && inputVal.length > 1) {
 				input.value = inputVal.replace(/.$/, btnVal);
 			}
-
-			decimalAdded = true;
 		} else if (btnVal === ".") {
-			if (decimalAdded == true) {
-				input.value += btnVal;
-				decimalAdded = false;
-			}
+			input.value += btnVal;
 		} else if (btnVal === "%") {
-			if (decimalAdded == true) {
-				input.value += btnVal;
-				decimalAdded = false;
-			}
+			input.value += btnVal;
 		} else if (btnVal === "x²") {
 			input.value += "^2";
-			decimalAdded = false;
 		} else if (btnVal === "xʸ") {
 			input.value += "^";
-			decimalAdded = false;
 		} else if (btnVal === "√x" && input.value !== "") {
 			input.value = `sqrt(${input.value})`;
-			decimalAdded = false;
 		} else if (btnVal === "abs" && input.value !== "") {
 			input.value = `abs(${input.value})`;
-			decimalAdded = false;
 		} else if (btnVal === "log" && input.value !== "") {
 			input.value = `ln(${input.value})`;
-			decimalAdded = false;
 		} else if (btnVal === "exp" && input.value !== "") {
 			input.value = `exp(${input.value})`;
-			decimalAdded = false;
 		} else if (btnVal === "sin" && input.value !== "") {
 			input.value = `sin(${input.value})`;
-			decimalAdded = false;
 		} else if (btnVal === "cos" && input.value !== "") {
 			input.value = `cos(${input.value})`;
-			decimalAdded = false;
 		} else if (btnVal === "tan" && input.value !== "") {
 			input.value = `tan(${input.value})`;
-			decimalAdded = false;
 		} else {
 			input.value += btnVal;
 		}
@@ -111,6 +88,15 @@ document.addEventListener("keydown", async (event) => {
 	const inputVal = input.value;
 	const lastChar = inputVal[inputVal.length - 1];
 
+	console.log(code);
+
+	if (
+		code.startsWith("Numpad") &&
+		event.getModifierState("NumLock") === false
+	) {
+		return;
+	}
+
 	if (
 		event.shiftKey == false &&
 		(code.startsWith("Numpad") || code.startsWith("Digit")) &&
@@ -125,7 +111,6 @@ document.addEventListener("keydown", async (event) => {
 		(code == "Equal" || code == "NumpadAdd")
 	) {
 		input.value += "+";
-		decimalAdded = true;
 	}
 
 	if (
@@ -133,7 +118,6 @@ document.addEventListener("keydown", async (event) => {
 		(code == "Minus" || code == "NumpadSubtract")
 	) {
 		input.value += "-";
-		decimalAdded = true;
 	}
 
 	if (
@@ -143,7 +127,6 @@ document.addEventListener("keydown", async (event) => {
 		(code == "Digit8" && event.shiftKey)
 	) {
 		input.value += "*";
-		decimalAdded = true;
 	}
 
 	if (
@@ -152,17 +135,23 @@ document.addEventListener("keydown", async (event) => {
 		(code == "Slash" || code == "IntlRo" || code == "NumpadDivide")
 	) {
 		input.value += "/";
-		decimalAdded = true;
 	}
 
-	if ((code == "Period" || code == "NumpadDecimal") && decimalAdded == true) {
+	if (code == "Period" || code == "NumpadDecimal" || code == "NumpadComma") {
 		input.value += ".";
-		decimalAdded = false;
+	}
+
+	if (
+		event.shiftKey == true &&
+		inputVal != "" &&
+		!operators.includes(lastChar) &&
+		code == "Quote"
+	) {
+		input.value += "^";
 	}
 
 	if (code == "KeyE") {
 		input.value += "e";
-		decimalAdded = false;
 	}
 
 	if (
@@ -172,7 +161,6 @@ document.addEventListener("keydown", async (event) => {
 		event.shiftKey
 	) {
 		input.value += "%";
-		decimalAdded = true;
 	}
 
 	if (
@@ -182,7 +170,6 @@ document.addEventListener("keydown", async (event) => {
 		event.shiftKey
 	) {
 		input.value += "^";
-		decimalAdded = true;
 	}
 
 	if (event.shiftKey == false && (code == "NumpadEnter" || code == "Enter")) {
@@ -191,7 +178,6 @@ document.addEventListener("keydown", async (event) => {
 
 	if (code == "Delete") {
 		input.value = "";
-		decimalAdded = true;
 	}
 
 	if (code == "Backspace") {
