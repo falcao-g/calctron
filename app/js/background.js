8b524b4243ea07129cdd479f8239d8c699068ef5
Falcão <gabriel.p.falcao50@gmail.com>
2022-08-08 11:28:48 -0400

    prettier again

--- app/js/background.js
+++ app/js/background.js
@@ -1,17 +1,17 @@
-const keys = document.getElementsByTagName("span");
-const input = document.getElementById("screen");
-const operators = ["+", "-", "*", "/", "^"];
-const math = require("mathjs");
+const keys = document.getElementsByTagName("span")
+const input = document.getElementById("screen")
+const operators = ["+", "-", "*", "/", "^"]
+const math = require("mathjs")
 
 for (const key of keys) {
 	key.addEventListener("click", async function (e) {
-		const inputVal = input.value;
-		const btnVal = this.textContent;
+		const inputVal = input.value
+		const btnVal = this.textContent
 
 		if (btnVal === "C") {
-			input.value = "";
+			input.value = ""
 		} else if (btnVal === "⌫") {
-			input.value = input.value.slice(0, -1);
+			input.value = input.value.slice(0, -1)
 		} else if (
 			(btnVal === "√" ||
 				btnVal === "√x" ||
@@ -28,87 +28,87 @@ for (const key of keys) {
 				btnVal === "¹/ₓ") &&
 			input.value === ""
 		) {
-			input.value = "";
+			input.value = ""
 		} else if (btnVal === "=") {
-			let equation = inputVal;
-			const lastChar = equation[equation.length - 1];
+			let equation = inputVal
+			const lastChar = equation[equation.length - 1]
 
 			if (operators.includes(lastChar) || lastChar === ".") {
-				equation = equation.replace(/.$/, "");
+				equation = equation.replace(/.$/, "")
 			}
 
 			if (equation) {
 				const result = await math
 					.evaluate(equation.replace(/π/g, "pi"))
-					.toString();
-				input.value = result;
+					.toString()
+				input.value = result
 			}
 		} else if (operators.includes(btnVal)) {
 			if (btnVal !== "-") {
-				const lastChar = inputVal[inputVal.length - 1];
+				const lastChar = inputVal[inputVal.length - 1]
 
 				if (inputVal !== "" && !operators.includes(lastChar)) {
-					input.value += btnVal;
+					input.value += btnVal
 				} else if (inputVal === "" && btnVal === "-") {
-					input.value += btnVal;
+					input.value += btnVal
 				}
 
 				if (operators.includes(lastChar) && inputVal.length > 1) {
-					input.value = inputVal.replace(/.$/, btnVal);
+					input.value = inputVal.replace(/.$/, btnVal)
 				}
 			} else {
-				input.value += btnVal;
+				input.value += btnVal
 			}
 		} else if (btnVal === ".") {
-			input.value += btnVal;
+			input.value += btnVal
 		} else if (btnVal === "%") {
-			input.value += btnVal;
+			input.value += btnVal
 		} else if (btnVal === "x²") {
-			input.value += "^2";
+			input.value += "^2"
 		} else if (btnVal === "xʸ") {
-			input.value += "^";
+			input.value += "^"
 		} else if (btnVal === "10ˣ") {
-			input.value += "10^";
+			input.value += "10^"
 		} else if (btnVal === "¹/ₓ" && input.value !== "") {
-			input.value = `1/(${input.value})`;
+			input.value = `1/(${input.value})`
 		} else if (btnVal === "√x" && input.value !== "") {
-			input.value = `sqrt(${input.value})`;
+			input.value = `sqrt(${input.value})`
 		} else if (btnVal === "abs" && input.value !== "") {
-			input.value = `abs(${input.value})`;
+			input.value = `abs(${input.value})`
 		} else if (btnVal === "log" && input.value !== "") {
-			input.value = `ln(${input.value})`;
+			input.value = `ln(${input.value})`
 		} else if (btnVal === "exp" && input.value !== "") {
-			input.value = `exp(${input.value})`;
+			input.value = `exp(${input.value})`
 		} else if (btnVal === "sin" && input.value !== "") {
-			input.value = `sin(${input.value})`;
+			input.value = `sin(${input.value})`
 		} else if (btnVal === "cos" && input.value !== "") {
-			input.value = `cos(${input.value})`;
+			input.value = `cos(${input.value})`
 		} else if (btnVal === "tan" && input.value !== "") {
-			input.value = `tan(${input.value})`;
+			input.value = `tan(${input.value})`
 		} else if (btnVal === "n!" && input.value !== "") {
-			input.value = `${input.value}!`;
+			input.value = `${input.value}!`
 		} else {
-			input.value += btnVal;
+			input.value += btnVal
 		}
 
-		e.preventDefault();
-	});
+		e.preventDefault()
+	})
 }
 
 /* Keyboard support */
 
 document.addEventListener("keydown", async (event) => {
-	const { code } = event;
-	const inputVal = input.value;
-	const lastChar = inputVal[inputVal.length - 1];
+	const { code } = event
+	const inputVal = input.value
+	const lastChar = inputVal[inputVal.length - 1]
 
-	console.log(code);
+	console.log(code)
 
 	if (
 		code.startsWith("Numpad") &&
 		event.getModifierState("NumLock") === false
 	) {
-		return;
+		return
 	}
 
 	if (
@@ -116,7 +116,7 @@ document.addEventListener("keydown", async (event) => {
 		(code.startsWith("Numpad") || code.startsWith("Digit")) &&
 		"0123456789".includes(code.slice(-1))
 	) {
-		input.value += code.slice(-1);
+		input.value += code.slice(-1)
 	}
 
 	if (
@@ -124,14 +124,14 @@ document.addEventListener("keydown", async (event) => {
 		!operators.includes(lastChar) &&
 		(code == "Equal" || code == "NumpadAdd")
 	) {
-		input.value += "+";
+		input.value += "+"
 	}
 
 	if (
 		!operators.includes(lastChar) &&
 		(code == "Minus" || code == "NumpadSubtract")
 	) {
-		input.value += "-";
+		input.value += "-"
 	}
 
 	if (
@@ -140,7 +140,7 @@ document.addEventListener("keydown", async (event) => {
 			(code == "KeyX" || code == "NumpadMultiply")) ||
 		(code == "Digit8" && event.shiftKey)
 	) {
-		input.value += "*";
+		input.value += "*"
 	}
 
 	if (
@@ -148,11 +148,11 @@ document.addEventListener("keydown", async (event) => {
 		!operators.includes(lastChar) &&
 		(code == "Slash" || code == "IntlRo" || code == "NumpadDivide")
 	) {
-		input.value += "/";
+		input.value += "/"
 	}
 
 	if (code == "Period" || code == "NumpadDecimal" || code == "NumpadComma") {
-		input.value += ".";
+		input.value += "."
 	}
 
 	if (
@@ -161,11 +161,11 @@ document.addEventListener("keydown", async (event) => {
 		!operators.includes(lastChar) &&
 		code == "Quote"
 	) {
-		input.value += "^";
+		input.value += "^"
 	}
 
 	if (code == "KeyE") {
-		input.value += "e";
+		input.value += "e"
 	}
 
 	if (
@@ -174,18 +174,18 @@ document.addEventListener("keydown", async (event) => {
 		code == "Digit5" &&
 		event.shiftKey
 	) {
-		input.value += "%";
+		input.value += "%"
 	}
 
 	if (event.shiftKey == false && (code == "NumpadEnter" || code == "Enter")) {
-		document.querySelector(".eval").click();
+		document.querySelector(".eval").click()
 	}
 
 	if (code == "Delete") {
-		input.value = "";
+		input.value = ""
 	}
 
 	if (code == "Backspace") {
-		input.value = input.value.slice(0, -1);
+		input.value = input.value.slice(0, -1)
 	}
-});
+})
