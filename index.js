b2d9733049491d13c7a6f84c8ae329c10566f0da
Falcão <gabriel.p.falcao50@gmail.com>
2022-08-08 11:29:05 -0400

    added a tray menu

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
-const contextMenu = require("electron-context-menu");
+const { app, BrowserWindow, Tray, Menu, globalShortcut } = require("electron")
+const contextMenu = require("electron-context-menu")
 
 // Support copy & paste
-contextMenu();
+contextMenu()
 
 app.on("ready", () => {
 	mainWindow = new BrowserWindow({
@@ -17,26 +17,61 @@ app.on("ready", () => {
 			contextIsolation: false,
 			enableRemoteModule: true,
 		},
-	});
+	})
 
-	Menu.setApplicationMenu(null);
+	Menu.setApplicationMenu(null)
 
-	mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+	mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
-	cientifica = false;
+	cientifica = false
 	globalShortcut.register("Alt+C", () => {
 		if (cientifica) {
-			mainWindow.setSize(335, 485);
-			mainWindow.loadURL(`file://${__dirname}/app/index.html`);
-			cientifica = false;
+			mainWindow.setSize(335, 485)
+			mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+			cientifica = false
 		} else {
-			mainWindow.setSize(410, 485);
-			mainWindow.loadURL(`file://${__dirname}/app/cientifica.html`);
-			cientifica = true;
+			mainWindow.setSize(410, 485)
+			mainWindow.loadURL(`file://${__dirname}/app/cientifica.html`)
+			cientifica = true
 		}
-	});
-});
+	})
+
+	tray = new Tray(__dirname + "/app/img/icon.png")
+	tray.setContextMenu(
+		Menu.buildFromTemplate([
+			{
+				label: "Padrão",
+				click: () => {
+					if (cientifica) {
+						mainWindow.setSize(335, 485)
+						mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+						cientifica = false
+					}
+				},
+			},
+			{
+				label: "Científica",
+				click: () => {
+					if (!cientifica) {
+						mainWindow.setSize(410, 485)
+						mainWindow.loadURL(`file://${__dirname}/app/cientifica.html`)
+						cientifica = true
+					}
+				},
+			},
+			{
+				type: "separator",
+			},
+			{
+				label: "Fechar",
+				click: () => {
+					app.quit()
+				},
+			},
+		])
+	)
+})
 
 app.on("window-all-closed", () => {
-	app.quit();
-});
+	app.quit()
+})
